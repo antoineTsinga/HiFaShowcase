1a321d0ec632d14002afbe69d6f5de476b5e7124
antoineTsinga <antoinetsinga@gmail.com>
2022-05-10 22:42:50 +0200

    AJout des service et controller (admin, client, cart, faq)

--- ShowcaseBackend/src/main/java/org/onyx/showcasebackend/Web/controllers/CartController.java
+++ ShowcaseBackend/src/main/java/org/onyx/showcasebackend/Web/controllers/CartController.java
@@ -1,7 +1,45 @@
 package org.onyx.showcasebackend.Web.controllers;
 
+import org.onyx.showcasebackend.Web.services.CartService;
+import org.onyx.showcasebackend.entities.Cart;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 public class CartController {
+    @Autowired
+    private CartService cartService;
+
+    @GetMapping("/carts")
+    private List<Cart> getAllCarts() {
+        return cartService.getCarts();
+    }
+
+    // creating a get mapping that retrieves the detail of a specific cart
+    @GetMapping("/carts/{id}")
+    private Cart getBooks(@PathVariable("id") long cartId) {
+        return cartService.getCartById(cartId);
+    }
+
+    // creating a deleted mapping that deletes a specified cart
+    @DeleteMapping("/carts/{id}")
+    private void deleteBook(@PathVariable("id") long cartId) {
+        cartService.deleteCart(cartId);
+    }
+
+    // creating post mapping that post the cart detail in the database
+    @PostMapping("/carts")
+    private long saveBook(@RequestBody Cart cart) {
+        cartService.saveCart(cart);
+        return cart.getId();
+    }
+
+    // creating put mapping that updates the cart detail
+    @PutMapping("/carts")
+    private Cart update(@RequestBody Cart cart) {
+        cartService.saveCart(cart);
+        return cart;
+    }
 
 }
--- ShowcaseBackend/src/main/java/org/onyx/showcasebackend/Web/controllers/FaqController.java
+++ ShowcaseBackend/src/main/java/org/onyx/showcasebackend/Web/controllers/FaqController.java
@@ -1,5 +1,45 @@
 package org.onyx.showcasebackend.Web.controllers;
 
+import org.onyx.showcasebackend.Web.services.FaqService;
+import org.onyx.showcasebackend.entities.Faq;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 public class FaqController {
+    @Autowired
+    private FaqService faqService;
+
+    @GetMapping("/faqs")
+    private List<Faq> getAllFaqs() {
+        return faqService.getFaqs();
+    }
+
+    // creating a get mapping that retrieves the detail of a specific faq
+    @GetMapping("/faqs/{id}")
+    private Faq getBooks(@PathVariable("id") long faqId) {
+        return faqService.getFaqById(faqId);
+    }
+
+    // creating a deleted mapping that deletes a specified faq
+    @DeleteMapping("/faqs/{id}")
+    private void deleteBook(@PathVariable("id") long faqId) {
+        faqService.deleteFaq(faqId);
+    }
+
+    // creating post mapping that post the faq detail in the database
+    @PostMapping("/faqs")
+    private long saveBook(@RequestBody Faq faq) {
+        faqService.saveFaq(faq);
+        return faq.getId();
+    }
+
+    // creating put mapping that updates the faq detail
+    @PutMapping("/faqs")
+    private Faq update(@RequestBody Faq faq) {
+        faqService.saveFaq(faq);
+        return faq;
+    }
 
 }
